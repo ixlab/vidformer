3f91c2c2f7e9005cb84cef042d522c9e399f7e84
Dominik Winecki <dominikwinecki@gmail.com>
2024-12-16 17:50:14 +0000

    Fix devcontainer script typo

--- scripts/deps_devcontainer.sh
+++ scripts/deps_devcontainer.sh
@@ -11,4 +11,4 @@ pip3 install -e ./vidformer-py
 
 # Download test video if it doesn't exist
 [ -f tos_720p.mp4 ] || curl -O https://f.dominik.win/data/dve2/tos_720p.mp4
-[-f apollo.jpg ] || curl -O https://f.dominik.win/data/dve2/apollo.jpg
+[ -f apollo.jpg ] || curl -O https://f.dominik.win/data/dve2/apollo.jpg
