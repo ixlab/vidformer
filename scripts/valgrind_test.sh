8b8ac410d6b02ab6812790bf151cecdba3d780b9
Dominik Winecki <dominikwinecki@gmail.com>
2025-01-10 01:21:21 +0000

    Re-enable full leak check

--- scripts/valgrind_test.sh
+++ scripts/valgrind_test.sh
@@ -4,11 +4,4 @@ set -e
 
 ./ffmpeg/build/bin/ffmpeg -y -i tos_720p.mp4 -c:v copy -c:a copy -t 30 tos_720p_short.mp4
 
-if [[ $(rustc --version) == *"1.83"* ]]; then
-    valgrind --error-exitcode=1 target/debug/vidformer-cli validate --name tos_720p_short --vid-path tos_720p_short.mp4 --stream 0
-else
-    # https://github.com/rust-lang/rust/issues/133574
-    echo "Hey there! Looks like stable rust finally got past 1.83 so hopefully valgrind full check works again."
-    echo "You need to re-add --leak-check=full to make CI pass again."
-    exit 1
-fi
+valgrind --error-exitcode=1 --leak-check=full target/debug/vidformer-cli validate --name tos_720p_short --vid-path tos_720p_short.mp4 --stream 0
