79b013bc3cee3fabae2be2a636d6719b6874a200
Dominik Winecki <dominikwinecki@gmail.com>
2025-01-22 00:06:25 +0000

    v0.10.1

--- vidformer-py/vidformer/__init__.py
+++ vidformer-py/vidformer/__init__.py
@@ -9,7 +9,7 @@ vidformer-py is a Python 🐍 interface for [vidformer](https://github.com/ixlab
 * [🧑‍💻 Source Code](https://github.com/ixlab/vidformer/tree/main/vidformer-py/)
 """
 
-__version__ = "0.10.0"
+__version__ = "0.10.1"
 
 
 import subprocess
